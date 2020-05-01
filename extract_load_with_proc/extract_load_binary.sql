26c6615d1a3a347c93bf39d8e4c57b3d495ad1f7
kimonas@gmail.com <kimonas@gmail.com>
2020-05-01 12:25:25 +0300

    path delimiter

--- extract_load_with_proc/extract_load_binary.sql
+++ extract_load_with_proc/extract_load_binary.sql
@@ -67,10 +67,17 @@ return @column_list;
 
 
 -- prints load command
-create procedure DBA.write_load_binary( owner varchar(256),tbl varchar(256),iq_export_dir varchar(1024),filename varchar(1024),out result varchar(32000) )                                                                                                       
-begin                                                                                                                                                                                                                                                            
+create or replace procedure DBA.write_load_binary( owner varchar(256),tbl varchar(256),iq_export_dir varchar(1024),filename varchar(1024),out result varchar(32000) )                                                                                                       
+begin    
+  DECLARE path_delimiter CHAR(1) ;
+  DECLARE platform varchar(20) ;
+  SELECT Value INTO platform from  sa_eng_properties() where PropName='Platform';
+  select   CASE left(lower(platform),3)
+  WHEN 'win' THEN  '\'
+  ELSE '/'
+  END into path_delimiter;                                                                                                                                                                                                                                                   
   select 'LOAD TABLE '+Table_owner+'.'+Table_name+'('+col_list_binary(Table_owner,Table_name)+')'                                                                                                                                                                
-    +'FROM '+"char"(39)+iq_export_dir+'\\'+filename+"char"(39)+"char"(10)                                                                                                                                                                                        
+    +'FROM '+"char"(39)+iq_export_dir+path_delimiter+filename+"char"(39)+"char"(10)                                                                                                                                                                                        
     +'format binary'+"char"(10)                                                                                                                                                                                                                                  
     +'on file error rollback'+"char"(10)                                                                                                                                                                                                                         
     +'notify 1000000'+"char"(10)                                                                                                                                                                                                                                 
@@ -83,7 +90,7 @@ begin
     where Table_owner = owner                                                                                                                                                                                                                                    
     and Table_name = tbl;                                                                                                                                                                                                                                        
   select result                                                                                                                                                                                                                                                  
-end                   
+end;                   
 
 call write_load_binary('DBA','k','C:\Temp\k','DBA.Actions.txt')
 
@@ -95,11 +102,18 @@ call write_load_binary('DBA','k','C:\Temp\k','DBA.Actions.txt')
 create or replace proc load_table_binary(owner varchar(256),tbl varchar(256),iq_export_dir varchar(1024),filename varchar(1024), out res varchar(20000))
 begin
 declare @dt datetime;
+DECLARE path_delimiter CHAR(1) ;
+DECLARE platform varchar(20) ;
+SELECT Value INTO platform from  sa_eng_properties() where PropName='Platform';
+select   CASE left(lower(platform),3)
+WHEN 'win' THEN  '\'
+ELSE '/'
+END into path_delimiter;   
 message '---------------------------------------------------' to client;
 message '---- Load Table '+owner+'.'+tbl+' --------' to client;
 select 'truncate table '+Table_owner+'.'+Table_name+';'+
 'LOAD TABLE '+Table_owner+'.'+Table_name+'('+col_list_binary(Table_owner,Table_name)+')'                                                                                                                                                                
-    +'FROM '+"char"(39)+iq_export_dir+'\\'+filename+"char"(39)+"char"(10)                                                                                                                                                                                        
+    +'FROM '+"char"(39)+iq_export_dir+path_delimiter+filename+"char"(39)+"char"(10)                                                                                                                                                                                        
     +'format binary'+"char"(10)                                                                                                                                                                                                                                  
     +'on file error rollback'+"char"(10)                                                                                                                                                                                                                         
     +'notify 1000000'+"char"(10)                                                                                                                                                                                                                                 
@@ -119,3 +133,7 @@ end;
 
  
 call load_table_binary('DBA','k','C:\Temp\k','DBA.Actions.txt')
+
+
+
+
