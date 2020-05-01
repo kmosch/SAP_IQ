9bbd89ceae35528a68c7b7d429208042fb4604c8
kmosch <46747178+kmosch@users.noreply.github.com>
2020-05-01 11:01:13 +0300

    Create create_cmd.sql

---
+++ various/create_cmd.sql
@@ -0,0 +1,22 @@
+create database '/sybdata/iq/kiq/kiq.db'
+blank padding on
+case respect
+page size 8192
+collation  '1253ELL'
+dba user 'dba'
+dba password 'sybase'
+iq path '/sybdata/iq/kiq/kiq.iq'
+iq size 500
+iq page size 131072
+temporary path '/sybdata/iq/kiq/kiq.iqtmp'
+temporary size 500;
+
+
+CREATE DBSPACE iq_main
+USING FILE iq_main_01
+'/sybdata/iq/kiq/iq_main_01' SIZE 512 MB IQ STORE;
+
+
+GRANT CREATE ON iq_main TO PUBLIC;
+REVOKE CREATE ON IQ_SYSTEM_MAIN FROM PUBLIC;
+SET OPTION PUBLIC.DEFAULT_DBSPACE = 'iq_main';
