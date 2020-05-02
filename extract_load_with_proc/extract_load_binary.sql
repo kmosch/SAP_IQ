25afb69f99f608e482bc655b74e768fd4828a780
kimonas@gmail.com <kimonas@gmail.com>
2020-05-02 09:35:10 +0300

    bcp first commit

--- extract_load_with_proc/extract_load_binary.sql
+++ extract_load_with_proc/extract_load_binary.sql
@@ -35,34 +35,34 @@ drop proc if exists "DBA"."col_list_binary";
 create FUNCTION "dba"."col_list_binary" (@table_owner varchar(100),@table_name varchar(100))
 returns varchar(32000)    
 as
-      declare @column_list varchar(32000),  
-              @table_id   int,
-			  @table_owner_id int,
-              @col_num    int,
-              @icount       int,
-              @temp_col varchar(4000)  
-     
-        select @table_owner_id=user_id(@table_owner)      
-        select @temp_col    = NULL
-        select @column_list = NULL
-        select @icount      = 1
-        select @table_id    =  table_id from SYSTABLE where table_name = @table_name and creator=@table_owner_id    
-        select @col_num = count(*) from  SYSCOLUMN where table_id= @table_id
-        while @icount <= @col_num
-              begin
-                    select @temp_col = column_name 
-                    from SYSCOLUMN 
-                    where (table_id = @table_id) and (column_id = @icount)
-					if @icount = @col_num
-                    begin
-                        select @column_list = @column_list + '['+ @temp_col + ']' + ' BINARY WITH NULL BYTE'+ char(10)
-                    end
-					else
-                    begin
-                        select @column_list = @column_list + '['+ @temp_col + ']'+ ' BINARY WITH NULL BYTE,'  + char(10)
-                    end
-                    select @icount = @icount + 1
-              end
+declare @column_list varchar(32000),  
+        @table_id   int,
+        @table_owner_id int,
+        @col_num    int,
+        @icount       int,
+        @temp_col varchar(4000)  
+
+select @table_owner_id=user_id(@table_owner)      
+select @temp_col    = NULL
+select @column_list = NULL
+select @icount      = 1
+select @table_id    =  table_id from SYSTABLE where table_name = @table_name and creator=@table_owner_id    
+select @col_num = count(*) from  SYSCOLUMN where table_id= @table_id
+while @icount <= @col_num
+      begin
+            select @temp_col = column_name 
+            from SYSCOLUMN 
+            where (table_id = @table_id) and (column_id = @icount)
+  if @icount = @col_num
+            begin
+                select @column_list = @column_list + '['+ @temp_col + ']' + ' BINARY WITH NULL BYTE'+ char(10)
+            end
+  else
+            begin
+                select @column_list = @column_list + '['+ @temp_col + ']'+ ' BINARY WITH NULL BYTE,'  + char(10)
+            end
+            select @icount = @icount + 1
+      end
 return @column_list;
 
 
